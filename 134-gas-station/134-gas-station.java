599f80747ce6a143a95fcfbf509e2413e105544e
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-04 13:49:29 -0600

    Time: 2 ms (94.65%), Space: 87.4 MB (5.10%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,25 +1,19 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // 剩余的油量。
-        int cur = 0;
-        // 如果不可达，缺多少油。
-        int lack = 0;
-        // 重试的起点，即不可达的点。
-        int retryIndex = 0;
 
-        for(int i = 0; i < gas.length; i ++) {
-            // 计算如果到达下一个点，剩多少油。
-            cur = cur + gas[i] - cost[i];
-            // cur < 0 时不可到达下一个点
-            if(cur < 0) {
-                // 要想到达，缺多少油
-                lack += -cur;
-                // 剩余油量重置为零，从下一个点开始重试。
-                cur = 0;
-                retryIndex = i + 1;
+        int len = gas.length;
+        int spare = 0;
+        int minSpare = Integer.MAX_VALUE;
+        int minIndex = 0;
+
+        for (int i = 0; i < len; i++) {
+            spare += gas[i] - cost[i];
+            if (spare < minSpare) {
+                minSpare = spare;
+                minIndex = i;
             }
         }
-        // 遍历完，看剩余油量能不能填补空缺。如果能，说明从重试节点开始，可以走完一圈。
-        return cur >= lack ? retryIndex : -1;
+
+        return spare < 0 ? -1 : (minIndex + 1) % len;
     }
 }
