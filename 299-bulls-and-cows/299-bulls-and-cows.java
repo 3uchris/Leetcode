16775aa5de6f7d1a7ee74505f0b75a9ab445d537
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-06 11:23:06 -0600

    Time: 5 ms (92.33%), Space: 42.6 MB (77.19%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.java
+++ 299-bulls-and-cows/299-bulls-and-cows.java
@@ -1,23 +1,23 @@
 class Solution {
     public String getHint(String secret, String guess) {
         int bulls = 0;
-        int cows = 0;
-        
-        int[] arr = new int[10];
+        int[] s_arr = new int[10];
+        int[] g_arr = new int[10];
         
         for(int i = 0; i < secret.length(); i++){
-            int s = secret.charAt(i) - '0';
-            int g = guess.charAt(i) - '0';
-            if(s == g){
+            if(secret.charAt(i) == guess.charAt(i)){
                 bulls++;
+            }else{
+                ++s_arr[secret.charAt(i) - '0'];
+                ++g_arr[guess.charAt(i) - '0'];
             }
-            else{
-                if(arr[s]++ < 0) cows++;
-                if(arr[g]-- > 0) cows++;
-            }
-            
         }
         
-        return bulls + "A" + cows + "B";
+        int cows = 0;
+        for(int i = 0; i < 10; i++){
+            cows += Math.min(s_arr[i], g_arr[i]);
+        }
+        return Integer.toString(bulls) + "A" + Integer.toString(cows) + "B"; 
     }
-}
+}    
+    
