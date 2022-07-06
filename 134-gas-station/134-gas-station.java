f6363bc52adfe893ed0b98db45db881b486a3fd4
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-06 13:19:24 -0600

    Time: 5 ms (20.69%), Space: 80.7 MB (42.22%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,19 +1,30 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        int i = 0; 
 
-        int len = gas.length;
-        int spare = 0;
-        int minSpare = Integer.MAX_VALUE;
-        int minIndex = 0;
+        // 从头到尾遍历每个加油站，并且检查以该加油站为起点，能否行驶一周
+        while(i < n){
+            int sumOfGas  = 0; // 总共加的油
+            int SumOfCost = 0; // 总共消费的油
+            int count = 0;     // 记录能走过几个站点
+            while(count < n){  // 退出循环的条件是走过所有的站点
+                int j = (i + count) % n; // 加油站是环形的
+                sumOfGas += gas[j];
+                SumOfCost += cost[j];
+                if(SumOfCost > sumOfGas){ // 如果这个站点发现油不够了
+                    break;
+                }
+                count++; // 这个站点满足情况
+            }
 
-        for (int i = 0; i < len; i++) {
-            spare += gas[i] - cost[i];
-            if (spare < minSpare) {
-                minSpare = spare;
-                minIndex = i;
+            if(count == n){  // 如果能环绕一圈
+                return i;
+            }else{ // 不行的话 从下一个站点开始 检查
+                i = i + count + 1;
             }
         }
-
-        return spare < 0 ? -1 : (minIndex + 1) % len;
+        // 所有加油站作为起点都不满足
+        return -1;
     }
 }
