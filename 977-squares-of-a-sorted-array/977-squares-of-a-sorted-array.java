601c3e4497e15189cf70ce4eab66e7e0e82669bc
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-07 14:04:37 -0600

    Time: 2 ms (84.58%), Space: 55.5 MB (63.04%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,10 +1,41 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] arr = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            arr[i] = nums[i] * nums[i];
+        //two pointer method
+        int n = nums.length;
+        int positive_pointer = 0;
+        
+        while(positive_pointer < n && nums[positive_pointer] < 0){
+            positive_pointer = positive_pointer + 1;
         }
-        Arrays.sort(arr);
+        
+        int negative_pointer = positive_pointer - 1;
+        int[] arr = new int[n];
+        int counter = 0;
+        
+        while(negative_pointer >= 0 && positive_pointer < n){
+            if(nums[negative_pointer] * nums[negative_pointer] < nums[positive_pointer] * nums[positive_pointer]){
+                arr[counter] = nums[negative_pointer] * nums[negative_pointer];
+                negative_pointer--;
+            }else{
+                arr[counter] = nums[positive_pointer] * nums[positive_pointer];
+                positive_pointer++;
+            }
+            counter++;
+        }
+        
+        while(negative_pointer >= 0){
+            arr[counter] = nums[negative_pointer] * nums[negative_pointer];
+            negative_pointer--;
+            counter++; 
+        }
+        
+        while(positive_pointer < n){
+            arr[counter] = nums[positive_pointer] * nums[positive_pointer];
+            positive_pointer++;
+            counter++; 
+        }
+        
         return arr;
+        
     }
 }
