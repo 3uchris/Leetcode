2b928767dfe3f6c698824d1726972841283a3f37
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-07 14:18:56 -0600

    Time: 2 ms (84.58%), Space: 55.8 MB (32.83%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,41 +1,19 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        //two pointer method
         int n = nums.length;
-        int positive_pointer = 0;
+        int i = 0, j = n - 1;
+        int[] result = new int[n];
         
-        while(positive_pointer < n && nums[positive_pointer] < 0){
-            positive_pointer = positive_pointer + 1;
-        }
-        
-        int negative_pointer = positive_pointer - 1;
-        int[] arr = new int[n];
-        int counter = 0;
-        
-        while(negative_pointer >= 0 && positive_pointer < n){
-            if(nums[negative_pointer] * nums[negative_pointer] < nums[positive_pointer] * nums[positive_pointer]){
-                arr[counter] = nums[negative_pointer] * nums[negative_pointer];
-                negative_pointer--;
-            }else{
-                arr[counter] = nums[positive_pointer] * nums[positive_pointer];
-                positive_pointer++;
+        for(int k = n-1; k >= 0; k--){
+            if(Math.abs(nums[i])>Math.abs(nums[j])){
+                result[k] = nums[i] * nums[i];
+                i++;
             }
-            counter++;
+            else{
+                result[k] = nums[j] * nums[j];
+                j--;
+            }
         }
-        
-        while(negative_pointer >= 0){
-            arr[counter] = nums[negative_pointer] * nums[negative_pointer];
-            negative_pointer--;
-            counter++; 
-        }
-        
-        while(positive_pointer < n){
-            arr[counter] = nums[positive_pointer] * nums[positive_pointer];
-            positive_pointer++;
-            counter++; 
-        }
-        
-        return arr;
-        
+        return result;
     }
 }
