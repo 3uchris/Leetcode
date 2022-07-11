fa80771c827ea5633e3e410699dd86c70256de5a
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-11 13:04:45 -0600

    Time: 14 ms (80.09%), Space: 51.9 MB (8.20%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,21 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        //show each element's apperance in the table
+        
+        //create a new hashmap named "bucket1"
+        //key:element of nums
+        //values: numbers of times the element appear
         HashMap<Integer, Integer> bucket1 = new HashMap<>();
         for(int num : nums){
-            bucket1.put(num, bucket1.getOrDefault(num, 0)+1); // count the numbers of times the element's apperance in the table 
+            bucket1.put(num, bucket1.getOrDefault(num, 0)+1);
         }
 
+        //create a new hashmap named "bucket2"
+        //key:numbers of times the element appears(==values of bucket1)
+        //values:list of elements
         HashMap<Integer, List<Integer>> bucket2 = new HashMap<>();
-        for(int num: bucket1.keySet()){
-            int elementFreq = bucket1.get(num);
-            if(!bucket2.containsKey(elementFreq)) bucket2.put(elementFreq, new ArrayList<>());
-            bucket2.get(elementFreq).add(num);
+        
+        
+        //.keySet :fetch the value mapped by a #particular# key mentioned in the parameter
+        for(int num: bucket1.keySet()){ 
+            //values of bucket1 equals frequency
+            int freq = bucket1.get(num);
+            if(!bucket2.containsKey(freq)) bucket2.put(freq, new ArrayList<>());
+            bucket2.get(freq).add(num);
         }
 
         int[] result = new int[k];
-        for(int i = nums.length; i > 0; i--){
-            if(bucket2.containsKey(i)){
+        for(int i = nums.length; i > 0; i--){ // going backward
+            if(bucket2.containsKey(i)){ // check frequncy 
                 List<Integer> list = bucket2.get(i);
                 for(int n : list){
                     result[--k] = n;
