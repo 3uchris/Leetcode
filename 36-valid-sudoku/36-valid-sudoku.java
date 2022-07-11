e14133cbccdd1bc2f1efbbadec2c06847258b5a2
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-11 14:32:51 -0600

    Time: 3 ms (85.70%), Space: 46.8 MB (56.25%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int [][]row  =new int[9][10];
-        int [][]col  =new int[9][10];
-        int [][]box  =new int[9][10];
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j]=='.'){
-                    continue;
-                }
-                int curNum = board[i][j]-'0';
-                if (row[i][curNum]==1){
-                    return false;
-                }if (col[j][curNum]==1){
-                    return false;
-                }
-                if (box[j/3 + (i/3) * 3][curNum]==1){
-                    return false;
-                }
-                row[i][curNum]=1;
-                col[j][curNum]=1;
-                box[j/3 + (i/3) * 3][curNum]=1;
-            }
+        int[][] row = new int[9][10];
+        int[][] column = new int[9][10];
+        int[][] box = new int[9][10];
+        
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.') continue; 
+                // if the value of the matrix is . then we skip this iteration
+                int curr = board[i][j] - '0';
+                if(row[i][curr] == 1) return false;
+                if(column[j][curr] == 1) return false;
+                if(box[j/3 +(i/3)*3][curr] == 1) return false;
+                
+                row[i][curr] = 1;
+                column[j][curr] = 1;
+                box[j/3+(i/3)*3][curr] = 1;
+                   
+            }   
         }
         return true;
     }
