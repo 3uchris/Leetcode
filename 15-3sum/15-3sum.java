0c960f94026e3c6ff3db91e0fd41dbe8a70cd137
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-12 12:26:43 -0600

    Time: 31 ms (66.02%), Space: 59.3 MB (44.75%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,39 +1,44 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
-        //we first define a arraylist that store our final result;
-        List<List<Integer>> arr = new ArrayList<>(); 
-        Arrays.sort(nums);                // sort the array
+        List<List<Integer>> arr = new ArrayList<>();
+        Arrays.sort(nums);
         int length = nums.length;
         
         for(int i = 0; i < length; i++){
-            if(nums[i] > 0) return arr;   //not possible to sum up to 0 if the smallest value is smaller than zero
+            
+            //counter case1: if the smallest element is larger than 0, not possible to sum up to 0
+            if(nums[i] > 0) return arr;
+            
+            //counter case2: must not contian duplicate triplets
             if(i > 0 && nums[i] == nums[i-1]) continue;
             
-            int curr= nums[i];
+            
+            int curr = nums[i];     //use two pointer to solve this question
             int left = i + 1;
             int right = length - 1;
             
-            while(left < right){
+            while(left<right){
                 int temp = curr + nums[left] + nums[right];
+                //case1 : sum = 0
                 if(temp == 0){
                     List<Integer> temp_arr = new ArrayList<>();
                     temp_arr.add(curr);
                     temp_arr.add(nums[left]);
                     temp_arr.add(nums[right]);
+                    
                     arr.add(temp_arr);
                     
-                    while(left < right && nums[left + 1] == nums[left]) ++left;
-                    while(left < right && nums[right - 1] == nums[right]) --right;
-                
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
                     ++left;
                     --right;
                 }
                 else if(temp < 0){
-                    ++left;
+                    left++;
                 }
                 else{
-                    --right;
+                    right--;
                 }
             }
         }
