73826126a17f68fd8818b581ae5682ebe11c9365
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-13 15:18:53 -0600

    Time: 5 ms (95.15%), Space: 43.5 MB (44.65%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,23 +1,29 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        if(s.length() == 0) return 0;
-        
-        int[] map = new int[26];
-        char[] chars = s.toCharArray();
-        
+        int length = s.length();
+        if(length == 0) return 0;
+
+        char[] charArray = s.toCharArray(); //在java当中遍历的时候要用到charAt()，所以先赋一个charArray
+        int[] freq = new int[26]; //string里面每个char出现的次数，只出现大写，所以26
+        int maxCount = 0; 
+
         int left = 0;
         int right = 0;
-        int max = 0;
-        
-        for(right = 0; right < s.length(); right++){
-            int index = chars[right] - 'A';
-            map[index]++;
-            max = Math.max(max, map[index]);
-            if(right - left + 1 > max + k){
-                map[chars[left]-'A']--;
-                left++;
+        int res = 0;
+        // [left,right)内最多替换k个letter的sub string
+
+        while(right < length){ //while loop 终止的条件是right抵达右壁
+            freq[charArray[right] - 'A']++; //freq有26个0，在那个position+1
+            maxCount = Math.max(maxCount, freq[charArray[right] - 'A']); 
+            right++; //右边界向右移动
+
+            if(right - left > maxCount + k){
+                freq[charArray[left] - 'A']--;
+                left++; // left向左移动一格
             }
+            res = Math.max(res, right - left);
+
         }
-        return chars.length - left;
+        return res;
     }
 }
