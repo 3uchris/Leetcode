65838179433437f486c3e3e86acf198684b02933
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-15 13:59:36 -0600

    Time: 3 ms (99.77%), Space: 44.7 MB (19.72%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,24 +1,30 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> operator = new Stack<>();
-        for (int i = 0; i < tokens.length; i++) {
-            if(tokens[i].matches("-?[0-9]+.*[0-9]*")){
-                operator.push(Integer.parseInt(tokens[i]));
-            } else{
-                // 如果遇到了操作符
-                // 先判断栈的容量，如果 > 2那么就进行操作，如果 < 2 就直接return 0;
-                if(operator.size() >= 2){
-                    int pre = operator.pop();
-                    int after = operator.pop();
-                    switch (tokens[i]){
-                        case "+" : operator.push(after + pre);break;
-                        case "-" : operator.push(after - pre);break;
-                        case "*" : operator.push(after * pre);break;
-                        case "/" : operator.push(after / pre);break;
-                    }
-                }
+        int[] ls = new int[tokens.length/2+1];
+        int index = 0;
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    ls[index - 2] = ls[index - 2] + ls[index - 1];
+                    index--;
+                    break;
+                case "-":
+                    ls[index - 2] = ls[index - 2] - ls[index - 1];
+                    index--;
+                    break;
+                case "*":
+                    ls[index - 2] = ls[index - 2] * ls[index - 1];
+                    index--;
+                    break;
+                case "/":
+                    ls[index - 2] = ls[index - 2] / ls[index - 1];
+                    index--;
+                    break;
+                default:
+                    ls[index++] = Integer.parseInt(token);
+                    break;
             }
         }
-        return operator.pop();
+        return ls[0];
     }
 }
