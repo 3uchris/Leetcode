1f9be339b64c5aa22b48e6b93239cc9b5652719e
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-15 11:48:17 -0600

    Time: 60 ms (24.96%), Space: 43.6 MB (37.60%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,26 +1,33 @@
 class Solution {
-    public static boolean checkInclusion(String s1, String s2) {
-        char[] str1 = s1.toCharArray();
-        char[] str2 = s2.toCharArray();
-        int len1 = s1.length();
-        int len2 = s2.length();
-        int[] charCount = new int[26]; // 【总欠账表】：s1的词频表
-        for (char c : str1) { // 统计s1的词频
-            charCount[c - 'a']++;
-        }
-        int l = 0, r = 0; // 滑动窗口左右边界
-        // 依次尝试固定以s2中的每一个位置l作为左端点开始的len1长度的子串s2[l ... l+len1)是否是s1的排列
-        while (l <= len2 - len1) { // 固定左端点只需要尝试到len2-len1即可
-            // 右边界s2[r]字符进入窗口【还账】
-            while (r < l + len1 && charCount[str2[r] - 'a'] >= 1) {
-                charCount[str2[r] - 'a']--; // 【"还账"】
-                r++;
-            }
-            if (r == l + len1) return true;
-            // 左边界s2[l]字符出窗口【赊账】，l++，开始尝试固定下一个位置做左端点
-            charCount[str2[l] - 'a']++; // 重新【"赊账"】
-            l++;
-        }
-        return false; // 所有的左端点均尝试还账失败，不可能再有答案了
+    public boolean checkInclusion(String s1, String s2) {
+    Map<Character, Integer> cnt = new HashMap<>();
+    for (char c : s1.toCharArray()) {
+        cnt.put(c, cnt.getOrDefault(c, 0) + 1);
     }
+
+    int matchedCnt = 0;
+    for (int i = 0; i < s2.length(); ++i) {
+        char c = s2.charAt(i);
+        if (cnt.containsKey(c)) {
+            cnt.put(c, cnt.get(c) - 1);
+            if (cnt.get(c) == 0) {
+                matchedCnt++;
+            }
+        }
+        if (i >= s1.length()) {
+            c = s2.charAt(i - s1.length());
+            if (cnt.containsKey(c)) {
+                if (cnt.get(c) == 0) {
+                    matchedCnt--;
+                }
+                cnt.put(c, cnt.get(c) + 1);
+            }
+        }
+
+        if (matchedCnt == cnt.keySet().size()) { 
+            return true;
+        }
+    }
+    return false;
+}
 }
