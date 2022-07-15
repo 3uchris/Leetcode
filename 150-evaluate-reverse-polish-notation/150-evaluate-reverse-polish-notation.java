1a87494e90101a0bde6d6988938486dd7ff39cb7
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-15 14:24:05 -0600

    Time: 4 ms (99.18%), Space: 45 MB (11.81%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,30 +1,30 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        int[] ls = new int[tokens.length/2+1];
+        int[] arr = new int[tokens.length/2+1];
         int index = 0;
-        for (String token : tokens) {
-            switch (token) {
+        for(String token : tokens){
+            switch(token){
                 case "+":
-                    ls[index - 2] = ls[index - 2] + ls[index - 1];
+                    arr[index - 2] = arr[index - 2] + arr[index - 1];
                     index--;
                     break;
                 case "-":
-                    ls[index - 2] = ls[index - 2] - ls[index - 1];
+                    arr[index - 2] = arr[index - 2] - arr[index - 1];
                     index--;
                     break;
                 case "*":
-                    ls[index - 2] = ls[index - 2] * ls[index - 1];
+                    arr[index - 2] = arr[index - 2] * arr[index - 1];
                     index--;
                     break;
                 case "/":
-                    ls[index - 2] = ls[index - 2] / ls[index - 1];
+                    arr[index - 2] = arr[index - 2] / arr[index - 1];
                     index--;
                     break;
                 default:
-                    ls[index++] = Integer.parseInt(token);
+                    arr[index++] = Integer.parseInt(token);
                     break;
             }
         }
-        return ls[0];
+        return arr[0];
     }
 }
