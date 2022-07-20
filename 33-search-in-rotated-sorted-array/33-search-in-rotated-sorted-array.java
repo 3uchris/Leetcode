3de12b6a063d456811e174544af885d3c7e8827b
Chris <77694377+3uchris@users.noreply.github.com>
2022-07-19 23:35:52 -0600

    Time: 1 ms (74.47%), Space: 42.5 MB (54.41%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,27 +1,25 @@
 class Solution {
-    public int search(int[] nums, int target) {
-    int minIdx = findMinIdx(nums);
-    if (target == nums[minIdx]) return minIdx;
-    int m = nums.length;
-    int start = (target <= nums[m - 1]) ? minIdx : 0;
-    int end = (target > nums[m - 1]) ? minIdx : m - 1;
-    
-    while (start <= end) {
-        int mid = start + (end - start) / 2;
-        if (nums[mid] == target) return mid;
-        else if (target > nums[mid]) start = mid + 1;
-        else end = mid - 1;
-    }
-    return -1;
-    }
-
-    public int findMinIdx(int[] nums) {
-        int start = 0, end = nums.length - 1;
-        while (start < end) {
-            int mid = start + (end -  start) / 2;
-            if (nums[mid] > nums[end]) start = mid + 1;
-            else end = mid;
+    public int search(int[] A, int target) {
+    int lo = 0;
+    int hi = A.length - 1;
+    while (lo < hi) {
+        int mid = (lo + hi) / 2;
+        if (A[mid] == target) return mid;
+        
+        if (A[lo] <= A[mid]) {
+            if (target >= A[lo] && target < A[mid]) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        } else {
+            if (target > A[mid] && target <= A[hi]) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
         }
-        return start;
+    }
+    return A[lo] == target ? lo : -1;
     }
 }
