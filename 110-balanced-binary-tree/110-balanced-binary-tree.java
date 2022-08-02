dc0e4136098516d1234ae7ae2af054af7327ec60
Chris <77694377+3uchris@users.noreply.github.com>
2022-08-02 15:29:39 -0600

    Time: 1 ms (99.10%), Space: 44.4 MB (51.83%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -14,32 +14,22 @@
  * }
  */
 class Solution {
-    private static final int UNBALANCED = -1;
-
     public boolean isBalanced(TreeNode root) {
-        return helper(root) != UNBALANCED;
+        return checkBalance(root) != -1; //not equals -1 means true, else false
     }
-
-    public int helper(TreeNode root) {
-        if (root == null)
-            return 0;
-
-        //如果左子节点不是平衡二叉树，直接返回UNBALANCED
-        int left = helper(root.left);
-        if (left == UNBALANCED)
-            return UNBALANCED;
-
-        //如果右子节点不是平衡二叉树，直接返回UNBALANCED
-        int right = helper(root.right);
-        if (right == UNBALANCED)
-            return UNBALANCED;
-
-        //如果左右子节点都是平衡二叉树，但他们的高度相差大于1，
-        //直接返回UNBALANCED
-        if (Math.abs(left - right) > 1)
-            return UNBALANCED;
-
-        //否则就返回二叉树中节点的最大高度
-        return Math.max(left, right) + 1;
+    
+    public int checkBalance(TreeNode root){
+        if(root == null) return 0;
+        
+        int left = checkBalance(root.left);
+        int right = checkBalance(root.right);
+        
+        if(left == -1) return -1;
+        if(right == -1) return -1;
+        if(Math.abs(left - right) > 1){
+            return -1;
+        }
+        
+        return Math.max(left,right)+1;
     }
 }
