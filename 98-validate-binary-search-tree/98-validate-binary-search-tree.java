6ba22aadefcf5b12fa3f59fe07c69cc3ad6cf12e
Chris <77694377+3uchris@users.noreply.github.com>
2022-08-04 16:51:32 -0600

    Time: 1 ms (62.14%), Space: 44 MB (42.69%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,29 +14,22 @@
  * }
  */
 class Solution {
+
     public boolean isValidBST(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        // 记录前一个节点
-        TreeNode pre = null;
-        
-        while(stack.size() > 0 || root != null){
-            // 一直向左子树走，每一次将当前节点保存到栈中
-            if(root != null){
-                stack.add(root);
-                root = root.left;
-            }
-            // 当前节点为空，证明走到了最左边，从栈中弹出节点
-            // 开始对右子树重复上述过程
-            else{
-                TreeNode cur = stack.pop();
-                // 判断序列是否有序
-                if(pre != null && cur.val <= pre.val){
-                    return false;
-                }
-                pre = cur;
-                root = cur.right;
-            }
+        if (root == null) return true;
+        return dfs(root, null, null);
+    }
+
+    private boolean dfs(TreeNode root, Integer min, Integer max) {
+        if (root == null) return true;
+
+        if (
+            (min != null && root.val <= min) || max != null && root.val >= max
+        ) {
+            return false;
         }
-        return true;
+
+        return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
     }
 }
+
