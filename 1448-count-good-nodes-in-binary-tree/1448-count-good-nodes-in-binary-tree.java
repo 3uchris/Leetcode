9ccd757cb414ddb77d94dfb5ffec6bd3ffc9769f
Chris <77694377+3uchris@users.noreply.github.com>
2022-08-04 15:47:04 -0600

    Time: 3 ms (75.66%), Space: 59.4 MB (69.01%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,29 +14,17 @@
  * }
  */
 class Solution {
-    int count = 0;
     public int goodNodes(TreeNode root) {
-        // test input
-        if (root==null) return count;
-        // start dfs
-        dfs(root, root.val);
-        return count;
+        return dfs(root, root.val);
     }
-    
-    private void dfs(TreeNode root, int val) {
-        if (root==null) return;
-        
-        if (root.val >= val) count++;
-        
-        //  In exampe 1, from path 3->1->3 we send
-        //     maximum value for comparision.
-        //     Why? Because if we send 1 (i.e. don't
-        //     send max value), we'll count 1 as the 
-        //     start of the path and increment count.
-        //     This is not correct because we are not
-        //     considering the already present maximum
-        //     value (i.e. 3)
-        dfs(root.left, Math.max(val, root.val));
-        dfs(root.right, Math.max(val, root.val));
+    public int dfs(TreeNode root, int max){
+        if(root == null) return 0;
+        max = Math.max(max, root.val);
+        if(root.val >= max){
+            return 1+dfs(root.left, max)+dfs(root.right,max);
+        }
+        else{
+            return dfs(root.left, max)+dfs(root.right, max);
+        }
     }
 }
