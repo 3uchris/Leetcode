b5e17e47cc776138689d7f8f25d444d9ec9821fb
Chris <77694377+3uchris@users.noreply.github.com>
2022-08-03 22:37:56 -0600

    Time: 2 ms (48.12%), Space: 42.4 MB (76.35%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,28 +14,25 @@
  * }
  */
 class Solution {
+
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if (root == null) {
-            return res;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-                if (i == size - 1) {  //将当前层的最后一个节点放入结果列表
-                    res.add(node.val);
-                }
+        List<Integer> list = new ArrayList<Integer>();
+        if (root == null) return list;
+        bfs(list, root);
+        return list;
+    }
+
+    public void bfs(List<Integer> list, TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode cur = q.poll();
+                if (i == 0) list.add(cur.val);
+                if (cur.right != null) q.offer(cur.right);
+                if (cur.left != null) q.offer(cur.left);
             }
         }
-        return res;
     }
 }
