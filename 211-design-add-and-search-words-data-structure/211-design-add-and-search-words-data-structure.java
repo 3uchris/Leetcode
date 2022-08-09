cdb5263766aba3ab735584805dfa4a5b168addb6
Chris <77694377+3uchris@users.noreply.github.com>
2022-08-09 14:42:31 -0600

    Time: 1295 ms (45.01%), Space: 253.6 MB (66.08%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -41,10 +41,3 @@ public class WordDictionary {
         return false;
     }
 }
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary obj = new WordDictionary();
- * obj.addWord(word);
- * boolean param_2 = obj.search(word);
- */
