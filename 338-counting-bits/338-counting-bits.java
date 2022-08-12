3256eab1d93a25002d6b34809ae2a89a893b30b4
Chris <77694377+3uchris@users.noreply.github.com>
2022-08-12 14:09:19 -0600

    Time: 3 ms (57.55%), Space: 47.8 MB (74.01%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,14 +1,19 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] bits = new int[n + 1];
-        int highBit = 0;
-        for (int i = 1; i <= n; i++) {
-            if ((i & (i - 1)) == 0) {
-                highBit = i;
-            }
-            bits[i] = bits[i - highBit] + 1;
+        int[] res = new int[n+1];
+        for(int i = 0; i < n+1; i++){
+            res[i] = countOnes(i);
         }
-        return bits;
+        return res;
     }
+    
+    public int countOnes(int n){
+        int count = 0;
+        while(n != 0){
+            count++;
+            n = n&(n-1); //make the last digit of binary number to be zero, and the assignment work as an increament.
+        }
+        return count;
+    }
+    
 }
-
