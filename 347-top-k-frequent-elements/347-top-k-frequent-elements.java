1082bd1851f938a83e29f9655bed10e1d5cc75db
Chris <77694377+3uchris@users.noreply.github.com>
2022-10-08 18:30:11 -0600

    Time: 11 ms (93.37%), Space: 44.5 MB (98.01%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,38 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         
-        //create a new hashmap named "bucket1"
-        //key:element of nums
-        //values: numbers of times the element appear
-        HashMap<Integer, Integer> bucket1 = new HashMap<>();
+        Map<Integer, Integer> hashMap1 = new HashMap<>();
         for(int num : nums){
-            bucket1.put(num, bucket1.getOrDefault(num, 0)+1);
+            hashMap1.put(num, hashMap1.getOrDefault(num, 0)+1);
         }
-
-        //create a new hashmap named "bucket2"
-        //key:numbers of times the element appears(==values of bucket1)
-        //values:list of elements
-        HashMap<Integer, List<Integer>> bucket2 = new HashMap<>();
         
+        Map<Integer, ArrayList<Integer>> hashMap2 = new HashMap<>();
         
-        //.keySet :fetch the value mapped by a #particular# key mentioned in the parameter
-        for(int num: bucket1.keySet()){ 
-            //values of bucket1 equals frequency
-            int freq = bucket1.get(num);
-            if(!bucket2.containsKey(freq)) bucket2.put(freq, new ArrayList<>());
-            bucket2.get(freq).add(num);
+        for(int num : hashMap1.keySet()){ // get key list
+            int freq = hashMap1.get(num);
+            if(!hashMap2.containsKey(freq)){
+                hashMap2.put(freq, new ArrayList<>());
+            }
+            hashMap2.get(freq).add(num);
         }
-
-        int[] result = new int[k];
-        for(int i = nums.length; i > 0; i--){ // going backward
-            if(bucket2.containsKey(i)){ // check frequncy 
-                List<Integer> list = bucket2.get(i);
-                for(int n : list){
-                    result[--k] = n;
-                    if(k==0) return result;
+        
+        int[] res = new int[k];
+        for(int i = nums.length; 0 < i; i--){
+            if(hashMap2.containsKey(i)){
+                ArrayList<Integer> arr = hashMap2.get(i);
+                for(int n : arr){
+                    res[--k] = n;
+                    if(k == 0){
+                        return res;
+                    }
                 }
             }
         }
-        return result;
+        return res;
     }
 }
